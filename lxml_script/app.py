25d95ac7e4a630e4d2f9275c87276d67f515729d
Ishank Sharma <45262592+ishank-dev@users.noreply.github.com>
2020-04-25 13:07:08 +0530

    add lxml script that supports broken xml files

---
+++ lxml_script/app.py
@@ -0,0 +1,18 @@
+from lxml import etree
+parser = etree.XMLParser(recover=True)
+tree = etree.parse('super.xml',parser)
+# search is case sensitive "africa" wont be detected
+keyword = "AFRICA"
+#Iterating over each tag called OBJECT
+for x in tree.iter('OBJECT'):
+    # Iterating over each WORD tag to locate the keyword
+    for i in x.iter('WORD'):
+        if(i.text == keyword):
+            '''
+                x[0].attrib['value'] is Descartes-TheGeometry_0273.djvu 
+                splitting twice at '.' and finding the last 4 lettes of the book name in this case it is 0273
+            '''
+            param = x[0].attrib['value'].split('.')[0] 
+            param = param[-4:]
+            
+            print("Keyword",i.text,"found at","page",param)
